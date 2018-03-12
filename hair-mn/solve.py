8797a381415fd5ddc35466e60ef334af1ce7dfb0
Allan Ma <allan@modiface.com>
2018-03-12 08:31:11 -0400

    train longer

--- hair-mn/solve.py
+++ hair-mn/solve.py
@@ -30,10 +30,9 @@ surgery.interp(solver.net, interp_layers)
 # scoring
 # test = np.loadtxt('../data/hair/test.txt', dtype=str)
 import glob
-test = sorted( [s.split('/')[-1].split('.jpg')[0] for s in glob.glob('../data/hair/realdata/test/*.jpg')] )[:100]
+test = sorted( [s.split('/')[-1].split('.jpg')[0] for s in glob.glob('../data/hair/realdata/test/*.jpg')] )
 
-for ind in range(50):
-    solver.step(500)
-    # N.B. metrics on the semantic labels are off b.c. of missing classes;
+for ind in range(100): # epoch=100
+    solver.step(4500/4). # every epoch has iterations = 4500 images/ batchsize4
     # score manually from the histogram instead for proper evaluation
     score.seg_tests(solver, './test_img' , test, layer='output_sep', gt='label')
