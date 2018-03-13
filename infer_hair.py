68041b7854a9c327abd4c33fddb4ac3fd8e2c917
Allan Ma <allan@modiface.com>
2018-03-13 10:31:55 -0400

    update deploy and use it to infer hair segmentation

--- infer_hair.py
+++ infer_hair.py
@@ -4,51 +4,70 @@ from PIL import Image
 import caffe
 import vis
 
-# the demo image is "2007_000129" from PASCAL VOC
-
-# load image, switch to BGR, subtract mean, and make dims C x H x W for Caffe
-image = Image.open('demo/image.jpg')
-image = image.resize([128, 128], Image.ANTIALIAS)
-in_ = np.array(image, dtype=np.float32)
-in_ = in_[:,:,::-1]
-# in_ -= np.array((104.00698793,116.66876762,122.67891434))
-in_ = in_.transpose((2,0,1))
-
-
-label = Image.open('demo/label.png')
-label = label.resize([128, 128],Image.NEAREST)
-label = np.array(label,np.uint8)
-# label -= 1  # rotate labels so classes start at 0, void is 255
-# label = label[np.newaxis, ...]
-label = (label > 200) * 1
-from scipy.misc import imsave
-imsave('demo/label_thr.png',label)
-
 # load net
-net = caffe.Net('hair-mn/deploy.prototxt', 'hair-mn/mobilenet_iter_25000.caffemodel', caffe.TEST)
-# shape for input (data blob is N x C x H x W), set data
-net.blobs['data'].reshape(1, *in_.shape)
-net.blobs['data'].data[...] = in_
-# run net and take argmax for prediction
-net.forward()
-out = net.blobs['output/relu/sep'].data[0]
+net = caffe.Net('hair-mn/deploy.prototxt', 'hair-mn/mobilenet_iter_110000.caffemodel', caffe.TEST)
+    
+def infer(input_image,input_mask,input_mask_thr,output_mask,output_overlay):
+    # the demo image is "2007_000129" from PASCAL VOC
 
-Dtype = out.dtype
-im = Image.fromarray(out[0,:,:])
-im = im.resize([6, 6], Image.NEAREST)
-im = np.array(im,Dtype)
-print im
+    # load image, switch to BGR, subtract mean, and make dims C x H x W for Caffe
+    image = Image.open(input_image)
+    image = image.resize([128, 128], Image.ANTIALIAS)
+    in_ = np.array(image, dtype=np.float32)
+    in_ = in_[:,:,::-1]
+    # in_ -= np.array((104.00698793,116.66876762,122.67891434))
+    in_ = in_.transpose((2,0,1))
 
-out = out.argmax(axis=0)
 
-im = Image.fromarray(out.astype(Dtype))
-im = im.resize([6, 6], Image.NEAREST)
-im = np.array(im,Dtype)
-print im
+    label = Image.open(input_mask)
+    label = label.resize([128, 128],Image.NEAREST)
+    label = np.array(label,np.uint8)
+    # label -= 1  # rotate labels so classes start at 0, void is 255
+    # label = label[np.newaxis, ...]
+    label = (label > 200) * 1
+    from scipy.misc import imsave
+    if input_mask_thr: imsave(input_mask_thr,label)
 
-# visualize segmentation in PASCAL VOC colors
-voc_palette = vis.make_palette(2)
-out_im = Image.fromarray(vis.color_seg(out, voc_palette))
-out_im.save('demo/output.png')
-masked_im = Image.fromarray(vis.vis_seg(image, out, voc_palette))
-masked_im.save('demo/visualization.jpg')
+    
+    # shape for input (data blob is N x C x H x W), set data
+    net.blobs['data'].reshape(1, *in_.shape)
+    net.blobs['data'].data[...] = in_
+    # run net and take argmax for prediction
+    net.forward()
+    out = net.blobs['output_sep'].data[0]
+
+    Dtype = out.dtype
+    im = Image.fromarray(out[0,:,:])
+    im = im.resize([6, 6], Image.NEAREST)
+    im = np.array(im,Dtype)
+    print im
+
+    out = out.argmax(axis=0)
+
+    im = Image.fromarray(out.astype(Dtype))
+    im = im.resize([6, 6], Image.NEAREST)
+    im = np.array(im,Dtype)
+    print im
+
+    # visualize segmentation in PASCAL VOC colors
+    voc_palette = vis.make_palette(2)
+    out_im = Image.fromarray(vis.color_seg(out, voc_palette))
+    out_im.save(output_mask)
+    masked_im = Image.fromarray(vis.vis_seg(image, out, voc_palette))
+    masked_im.save(output_overlay)
+    
+import glob
+
+image_files = sorted(glob.glob('demo/*.jpg'))
+mask_files = sorted(glob.glob('demo/*.png'))
+
+for input_image, input_mask in zip(image_files,mask_files):
+    
+    # input_mask_thr = input_mask.split('.jpg')[0]+'_thr.png'
+    
+    output_mask = input_mask.split('.jpg')[0]+'_output.png'
+    
+    output_overlay = input_image.split('.jpg')[0]+'_overlay.jpg'
+    
+    infer(input_image,input_mask,None,output_mask,output_overlay)
+    
